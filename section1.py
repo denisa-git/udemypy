502fc0188401179e4b6af1df33df8ded44ac77f8
denisa-git <28634418+denisa-git@users.noreply.github.com>
2025-01-10 23:57:59 +0000

    section1

---
+++ section1.py
@@ -0,0 +1,40 @@
+#ce1
+print("Hello World")
+
+# ce2
+print("i want a job")
+
+#ce3
+name = "John"
+city = "New York"
+country = "USA"
+
+#ce4
+firstname = "John"
+middlename = "Name"
+lastname = "Name"
+person = [firstname, middlename, lastname]
+
+#ce5
+firstname = 'John'
+middlename = 'Jack'
+lastname = 'Smith'
+
+person = [firstname, middlename, lastname, "New York"]
+print(person)
+
+#ce6
+w1 = "1"
+w2 = "2" 
+w3 = "w"
+
+words = [w1,w2,w3]
+print(words)
+
+#ce7
+list = ["list","of","items"]
+print(type(list))
+
+#ce8
+title = input("Enter title: ")
+print("title length is",len(title))
