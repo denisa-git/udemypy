97cca4c584e2bf84a683160d921517edc00e781e
denisa-git <28634418+denisa-git@users.noreply.github.com>
2025-01-11 02:06:54 +0000

    section2

---
+++ section2.py
@@ -0,0 +1,56 @@
+# ce8
+# while True:
+#     print("Hello")
+
+# try
+# prompt = "enter item - "
+# list = []
+# while True:
+#     todo = input(prompt)
+#     print(todo.capitalize())
+#     list.append(todo)
+#     print(list)
+
+# e9
+student = "John Smith"
+students = []
+students.append(student)
+print(students)
+
+# e10
+person = "sarah"
+print(person.capitalize())
+
+# e11
+button_label = "submit"
+print(button_label.capitalize())
+
+# e12
+sentence = "the sky is blue today."
+print(sentence.capitalize())
+name = "sarah smith"
+print(name.title())
+
+# try
+statement = input("am i ok? ")
+while statement != "no":
+    statement = input("am i ok? ")
+print("thought so")
+
+statement = input("do you love me? ")
+while statement != "YES":
+    statement = input("do you looove me? ")
+print("ty :)")
+
+# try by myself
+x = 0
+while x < 6:
+    x = x + 1
+    print(x)
+
+var = 0
+list = []
+while (var < 6):
+    var = var + 1
+    list.append(var)
+print(list)
