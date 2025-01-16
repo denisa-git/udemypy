ee27caf66badd30e73d949f2ae698bc4a93f81a0
denisa-git <28634418+denisa-git@users.noreply.github.com>
2025-01-16 17:46:02 +0000

    section4

---
+++ section4.py
@@ -0,0 +1,37 @@
+#try
+# todolist = []
+
+# while True:
+#     action = input("add, edit, remove, show or exit to do list? ")
+
+#     match action:
+#         case 'add':
+#             todo = input("what do you want to do? ")
+#             todolist.append(todo)
+#         case 'edit':
+#             edit = int(input("which item to edit? "))
+#             if edit <= len(todolist):
+#                 todolist[edit-1] = input("new to do: ")
+#             else:
+#                 print("learn how to count too")
+#         case 'remove':
+#             remove = int(input("which item to remove? "))
+#             if remove <= len(todolist):
+#                 todolist.remove(todolist[remove-1])
+#             else:
+#                 print("eeeeeeehhh wrong!!!!! stupid")
+#         case 'show':
+#             for item in todolist:
+#                 print(item)
+#         case 'exit':
+#             break
+# print("see ya")
+
+#e19
+# serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+# print(serials[2])
+
+#e20
+# words = ['w' , 'o' , 'r' , 'd' , 's']
+# print(words[3])
+
